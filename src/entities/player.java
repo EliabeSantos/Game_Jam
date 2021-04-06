26aa3aa208736f1affd1b1646bf88df12562a906
eliabe <eliabedodo@gmail.com>
2021-04-05 22:33:13 -0300

    animacao de caminhada adicionada e logica comecada

--- src/entities/player.java
+++ src/entities/player.java
@@ -5,7 +5,8 @@ import java.awt.image.BufferedImage;
 
 public class player extends entity{
 
-	
+	public int right_dir = 0, left_dir = 1;
+	public int dir = 0;
 	
 	public boolean right, left, up, down;
 	public double speed = 0.9;
@@ -19,24 +20,34 @@ public class player extends entity{
 		
 		rightPlayer = new BufferedImage[4];
 		leftPlayer = new BufferedImage[4];
-		rightPlayer[0] =  Main.Main.spritesheet.getSprite(X, Y, WIDTH, HEIGHT);
-//		rightPlayer[0] =  Main.spritesheet.getSprite(32, 0, 16, 16);
+		
+		for(int i = 0; i < 4;i++) {
+			rightPlayer[i] =  Main.Main.spritesheet.getSprite(32 + (i*16), 0, 16, 16);
+		}
+		for(int i = 0; i < 4;i++) {
+			leftPlayer[i] =  Main.Main.spritesheet.getSprite(32 + (i*16), 16, 16, 16);
+		}
 	}
 	
 	public void tick() {
 		if(right) {
+			dir = right_dir;
 			x+=speed;
 		}else if(left) {
 			x-=speed;
+			dir = left_dir;
 		}
 		if(up) {
 			y-=speed;
 		}else if(down) {
 			y+=speed;
 		}
-
 	}
 	public void render(Graphics g) {
-		
+		if(dir == right_dir) {
+			g.drawImage(rightPlayer[0],this.getX(), this.getY(), null);
+		}else if(dir == left_dir) {
+			g.drawImage(leftPlayer[3],this.getX(), this.getY(), null);
+		}
 	}
 }
